071a9908111d32989a1dc9bcb8f21a59fb83e16c
Jay Shridharani <jshridha@gmail.com>
2015-07-29 23:10:06 -0400

    Update myopenhab configuration

--- files/scripts/download_openhab.sh
+++ files/scripts/download_openhab.sh
@@ -12,7 +12,7 @@ else
   wget --quiet --no-check-certificate --no-cookies -O /tmp/demo-openhab.zip https://bintray.com/artifact/download/openhab/bin/distribution-$OPENHAB_VERSION-demo-configuration.zip
 fi
 
-wget --quiet --no-check-certificate --no-cookies -O /tmp/org.openhab.io.myopenhab-1.4.0-SNAPSHOT.jar https://my.openhab.org/downloads/org.openhab.io.myopenhab-1.4.0-SNAPSHOT.jar
+wget --quiet --no-check-certificate --no-cookies -O /tmp/org.openhab.io.myopenhab-1.7.0.jar https://bintray.com/artifact/download/openhab/mvn/org/openhab/io/org.openhab.io.myopenhab/1.7.0/org.openhab.io.myopenhab-1.7.0.jar
 wget --quiet --no-check-certificate --no-cookies -O /tmp/hyperic-sigar-1.6.4.tar.gz http://downloads.sourceforge.net/project/sigar/sigar/1.6/hyperic-sigar-1.6.4.tar.gz
 
 rm -rf /opt/openhab
@@ -24,6 +24,6 @@ unzip -q -d /opt/openhab /tmp/distribution-runtime.zip
 unzip -q -d /opt/openhab/addons-available /tmp/distribution-addons.zip
 unzip -q -d /opt/openhab/demo-configuration /tmp/demo-openhab.zip
 chmod +x /opt/openhab/start.sh
-mv /tmp/org.openhab.io.myopenhab-1.4.0-SNAPSHOT.jar /opt/openhab/addons-available
+mv /tmp/org.openhab.io.myopenhab-1.7.0.jar /opt/openhab/addons-available
 mv /opt/openhab/configurations /etc/openhab
 ln -s /etc/openhab /opt/openhab/configurations
